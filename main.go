fc338d78a38117a7c8d08e60ee429f7e11816bd6
konoplyannikovDV <konoplyannikov.d.v@gmail.com>
2025-05-04 00:18:15 +0300

    Добавлена функция pow

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"math"
+)
 
+func pow(a, b float64) float64 {
+	return math.Pow(a, b)
+}
+func sqrt(a float64) (float64, error) {
+	if a < 0 {
+		return 0, errors.New("sqrt of negative")
+	}
+	return math.Sqrt(a), nil
 }
